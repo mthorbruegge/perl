3cdcf458ae0e4938ebcfa183aea32bdbdbd3ccd8
Marco Thorbruegge <marco.thorbruegge@gmx.de>
2013-07-03 15:35:54 +0300

    Added placeholders/comments to prepare for "Only new entries" setting

--- epso.pl
+++ epso.pl
@@ -16,7 +16,8 @@ my $counter = 0;
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $date = ($year+1900).(sprintf("%02d",$mon+1)).(sprintf("%02d",$mday))."-".(sprintf("%02d",$hour)).":".(sprintf("%02d",$min)).":".(sprintf("%02d",$sec));
 
-my $textfilename = $path."/epso/text/".$date."-epso-AD.txt";
+my $textfileFULL = $path."/epso/text/".$date."-epso-AD-FULL.txt";
+my $textfileONLYNEW = $path."/epso/text/".$date."-epso-AD-NEW.txt";
 
 my $htmlcontent = get("http://europa.eu/epso/apply/jobs/temp/index_en.htm") or die "Error while retrieving page: $!\n";
 my $textcontent = HTML::FormatText->format_string($htmlcontent);
@@ -44,7 +45,10 @@ while ($hashstore[$hashstorecounter] = <FILE>) {
 close(FILE);
 
 my $skipfirst = 0;
-my @temparray = ();
+my @temparrayFULL = ();
+
+# TODO: initialize the array for only the new records
+# my $temparrayONLYNEW = ();
 
 ## Variables for creation of a hash over one set of text
 
@@ -78,14 +82,18 @@ while (defined $textarray[$counter]){
                 }
                 $newhash = Digest::SHA->sha256_hex(@hashtemp);
                 if (&comparehash($newhash)) { 
-                    push(@temparray,"***** NEW *****");
+                    push(@temparrayFULL,"***** NEW *****");
+
+# TODO: routine to fill the array with text for only new entries
+# to be done in this 'if' statement!
+
                 }
-                push(@temparray,$agency);
+                push(@temparrayFULL,$agency);
                 while ($textarray[$counter] ne ""){
-                    push(@temparray,$textarray[$counter]);
+                    push(@temparrayFULL,$textarray[$counter]);
                     $counter++;
                 }
-                push(@temparray,"");
+                push(@temparrayFULL,"");
                 @hashtemp = ();
             }
         }
@@ -93,37 +101,52 @@ while (defined $textarray[$counter]){
     $counter++;
 }
 
-open (FILE, ">>".$textfilename) or die "Error while opening file for Text storage: $!\n";                                                                                                                                                  
+open (FILE, ">>".$textfileFULL) or die "Error while opening file for Text storage: $!\n";                                                                                                                                                  
 binmode(FILE, ":utf8");                                                                                                                                                                                                                    
 
 $counter = 0;
 
-while (defined $temparray[$counter]){                                                                                                                                                                                                      
-    $temparray[$counter] =~ s/^\s+//;                                                                                                                                                                                                  
-    print FILE $temparray[$counter]."\n";                                                                                                                                                                                              
+while (defined $temparrayFULL[$counter]){                                                                                                                                                                                                      
+    $temparrayFULL[$counter] =~ s/^\s+//;                                                                                                                                                                                                  
+    print FILE $temparrayFULL[$counter]."\n";                                                                                                                                                                                              
     $counter++;                                                                                                                                                                                                                        
 }                                                                                                                                                                                                                                          
 
 close(FILE);
 
+# TODO: here the routine to write only new records to another file
+# open (FILE, ">>".$textfileONLYNEW) or die "Error while opening file for Text storage: $!\n";                                                                                                                                                  
+# binmode(FILE, ":utf8");                                                                                                                                                                                                                    
+#
+# $counter = 0;
+#
+# while (defined $temparrayFULL[$counter]){                                                                                                                                                                                                      
+#     $temparrayFULL[$counter] =~ s/^\s+//;                                                                                                                                                                                                  
+#     print FILE $temparrayFULL[$counter]."\n";                                                                                                                                                                                              
+#     $counter++;                                                                                                                                                                                                                        
+# }                                                                                                                                                                                                                                          
+#
+# close(FILE);
+
+
 sub comparehash {
 
 # compare the calculated hash sum of the actual text array with $counter++; # all previous hashes from the hash-file
 
-my $localcounter = 0; 
-my $localstring = $_[0];
-while ($hashstore[$localcounter]) {
-    if ($hashstore[$localcounter] eq $localstring) {
-        return undef;
-     }
-    $localcounter++;
-}
-open (FILE, ">>".$hashstorefile) or die "Error while opening file for Text storage: $!\n";
-binmode(FILE, ":utf8");
-print FILE $_[0]."\n";
-close(FILE);
-$hashstore[$localcounter] = $localstring;
-return 1;
+    my $localcounter = 0; 
+    my $localstring = $_[0];
+    while ($hashstore[$localcounter]) {
+        if ($hashstore[$localcounter] eq $localstring) {
+            return undef;
+        }
+        $localcounter++;
+    }
+    open (FILE, ">>".$hashstorefile) or die "Error while opening file for Text storage: $!\n";
+    binmode(FILE, ":utf8");
+    print FILE $_[0]."\n";
+    close(FILE);
+    $hashstore[$localcounter] = $localstring;
+    return 1;
 }
 
 exit 0;
